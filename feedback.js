ff3eb22c2db0b77c48a011f4f33f474f81abb8be
eda-lahari <151444228+eda-lahari@users.noreply.github.com>
2024-10-26 06:06:23 +0530

    Update feedback.js

--- feedback.js
+++ feedback.js
@@ -1,9 +1,5 @@
 document.getElementById('feedbackForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-
-    // Display the thank-you message
+    event.preventDefault();
     document.getElementById('thank-you-message').style.display = 'block';
-
-    // Optionally, clear the form fields after submission
     document.getElementById('feedbackForm').reset();
 });
